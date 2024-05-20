fed06ee6b653cd1f77a89d6b08e589d36534eeac
Tomas Pesek <pesekt@gmail.com>
2024-05-20 22:28:30 +0200

    mocking matchMedia function in the global window object

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom/vitest";
+
+// Mock matchMedia
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: () => {},
+      removeListener: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => false,
+    };
+  };
